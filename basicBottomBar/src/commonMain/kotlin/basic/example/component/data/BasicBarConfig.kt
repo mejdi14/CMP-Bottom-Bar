1199d49c73da312db52e84deccc6ec1472b48c95
mejdi14 <mejdiclubiste@gmail.com>
2024-10-14 12:18:25 +0200

    there are some changes to do in the tiny module

---
+++ basicBottomBar/src/commonMain/kotlin/basic/example/component/data/BasicBarConfig.kt
@@ -0,0 +1,19 @@
+package basic.example.component.data
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import org.example.core.bottombar.data.BottomBarHoverText
+
+data class BasicBarConfig (
+    val itemsRadius: Dp = 10.dp,
+    val onSelectItemSizeChangeFriction: Float = 1.3f,
+    val onSelectItemSizeChangeDurationMillis: Int = 300,
+    val hoverCancelDurationMillis: Long = 8,
+    val itemSeparationSpace: Dp = 10.dp,
+    val parentAndSubVerticalSeparationSpace: Dp = 10.dp,
+    val marginForScreenSizeChanges: Float = 10f,
+    val hoverTextConfig: BottomBarHoverText = BottomBarHoverText()
+)
