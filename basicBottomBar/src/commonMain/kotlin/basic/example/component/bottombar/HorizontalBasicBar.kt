f297c505814d72f643e75dd6883951eac1cd7eb9
mejdi14 <mejdiclubiste@gmail.com>
2024-10-17 22:55:56 +0200

    save progress

--- basicBottomBar/src/commonMain/kotlin/basic/example/component/bottombar/HorizontalBasicBar.kt
+++ basicBottomBar/src/commonMain/kotlin/basic/example/component/bottombar/HorizontalBasicBar.kt
@@ -1,6 +1,8 @@
 package basic.example.component.bottombar
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.LazyRow
@@ -18,6 +21,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.State
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -87,15 +91,18 @@ internal fun HorizontalBasicBar(
             ) {
                 itemsIndexed(bottomBarItems) { index, item ->
 
-                    IconButton(
-                        onClick = {
-                            selectedIndex.value = index
-                            onIconClick(item)
-                        },
-                        modifier = Modifier.align(Alignment.Center)
+                    Box(
+                        modifier = Modifier.size(basicBarConfig.itemSize).align(Alignment.Center)
                             .hoverEffect { onHover ->
                                 isHovered.value = onHover
                                 hoverSelectedIndex.value = index
+                            }
+                            .clickable(
+                                interactionSource = remember { MutableInteractionSource() },
+                                indication = null
+                            ){
+                                selectedIndex.value = index
+                                onIconClick(item)
                             }
                             .background(
                                 color = if (isHovered.value
