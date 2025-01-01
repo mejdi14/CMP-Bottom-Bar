e5e32aaf9883c473ea38fcf57bfa4acfddea5704
mejdi14 <mejdiclubiste@gmail.com>
2025-01-01 17:30:21 +0100

    row of boxes

---
+++ gooeyBottomBar/src/commonMain/kotlin/org/example/gooey/GooeyBottomBar.kt
@@ -0,0 +1,17 @@
+package org.example.gooey
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun GooeyBottomBar() {
+    Row {
+        Box(modifier = Modifier.width(200.dp).height(70.dp)) {
+        }
+    }
+}
