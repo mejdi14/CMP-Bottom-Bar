2f0c3bc99e8ded3aa2aa2cb74c6475252aa98663
mejdi14 <mejdiclubiste@gmail.com>
2025-02-17 10:47:36 +0100

    save and ship

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,5 @@ plugins {
     alias(libs.plugins.jetbrainsCompose) apply false
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
+    id("com.vanniktech.maven.publish") version "0.30.0"
 }
