9c90caf8097c1c2830b218f58bd0c5711721d27f
mejdi14 <mejdiclubiste@gmail.com>
2025-02-18 13:44:41 +0100

    one more library and fix colors

--- aztopiaBottomBar/src/iosMain/kotlin/org/example/aztopia/MainViewController.kt
+++ aztopiaBottomBar/src/iosMain/kotlin/org/example/aztopia/MainViewController.kt
@@ -2,4 +2,4 @@ package org.example.aztopia
 
 import androidx.compose.ui.window.ComposeUIViewController
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController { }
